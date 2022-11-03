355c3d0c50882ad372fff0b85e19d41d53bbdfd9
Juan Araujo <araujogreydiego@gmail.com>
2022-11-02 20:30:29 -0400

    Basic post navigation

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -4,11 +4,15 @@ export async function getPostListings() {
   return prisma.post.findMany({
     select: {
       slug: true,
-      title: true
-    }
-  })
+      title: true,
+    },
+  });
 }
 
 export async function getPosts() {
   return prisma.post.findMany();
+}
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } });
 }
